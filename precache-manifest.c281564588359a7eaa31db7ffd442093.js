e5b853335fd0c40762a98d06c9cef3296b5ecab4
AxlRicci <alexbricci@gmail.com>
2020-08-03 19:13:12 -0400

    Updates

---
+++ precache-manifest.c281564588359a7eaa31db7ffd442093.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ace65561f891dc8c1fcabcfd3cc330d",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "f10d4016ce685ef0b89f",
+    "url": "/monsters-rolodex/static/css/main.a90f7103.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f10d4016ce685ef0b89f",
+    "url": "/monsters-rolodex/static/js/main.91299fed.chunk.js"
+  },
+  {
+    "revision": "f051cecc4ee3cb3b301f",
+    "url": "/monsters-rolodex/static/js/runtime-main.eea62ddf.js"
+  }
+]);
